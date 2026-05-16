ac37cb76fc9a644db0dfc0f19f13ddea89a24190
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-15 22:08:13 -0400

    09 finished

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let start = Math.min(x,y);
+    let end = Math.max(x,y);
 
+    let sum = 0;
+    for(let i = start; i <=end; i++)
+        sum+= i;
+
+    return sum;
 };
 
 // Do not edit below this line
