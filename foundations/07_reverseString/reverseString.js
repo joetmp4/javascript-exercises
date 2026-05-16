9b815d7a05663ded24f9cd63d5252ab1aa8eba98
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-15 21:39:47 -0400

    finished 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    let length = string.length;
+    for (let i = 0; i < length; i++)
+        reversed[i] = string[length - i - 1];
+    return reversed;
 };
 
 // Do not edit below this line
